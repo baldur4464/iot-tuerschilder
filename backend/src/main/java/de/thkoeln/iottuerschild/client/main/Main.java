7818d961b0507215121499e8ab46558429a743ce
Patrick Schmidt <patrick.schmidt54@gmail.com>
2022-05-30 12:03:10 +0200

    Connection gefixxt, dotenv dependency hinzugefügt, Broker und ClientID werden nun aus der .env Datei entnommen

--- backend/src/main/java/de/thkoeln/iottuerschild/client/main/Main.java
+++ backend/src/main/java/de/thkoeln/iottuerschild/client/main/Main.java
@@ -3,10 +3,8 @@ package de.thkoeln.iottuerschild.client.main;
 import de.thkoeln.iottuerschild.client.database.Database;
 import de.thkoeln.iottuerschild.client.mqttnachricht.MQTTNachricht;
 import de.thkoeln.iottuerschild.client.publisher.Publisher;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
@@ -27,6 +25,7 @@ public class Main {
             System.out.println("Testprogramm zum Versenden von Nachrichten");
             System.out.println("[1] Sende eine Testnachricht");
             System.out.println("[0] Beende das Programm");
+            System.out.print("Eingabe: ");
 
 
             try {
@@ -40,7 +39,6 @@ public class Main {
             switch (auswahl) {
                 case 1:
                     MQTTNachricht nachricht = new MQTTNachricht("Test", "Raum/5", "12:00", "13:00", "5");
-                    nachricht.nachrichtToJSON();
                     pub.sendNachricht(nachricht.getTopic(), nachricht.nachrichtToJSON().toString(), 0);
                     break;
                 case 2:
--- backend/src/main/java/de/thkoeln/iottuerschild/client/publisher/Publisher.java
+++ backend/src/main/java/de/thkoeln/iottuerschild/client/publisher/Publisher.java
@@ -2,10 +2,14 @@ package de.thkoeln.iottuerschild.client.publisher;
 
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import io.github.cdimascio.dotenv.*;
 
 public class Publisher {
 
     private static Publisher instance;
+    private MqttClient client;
+    private final MemoryPersistence persistence;
+    private Dotenv dotenv;
 
     public static Publisher getInstance() {
         if(instance == null) {
@@ -15,37 +19,34 @@ public class Publisher {
     }
 
     private Publisher() {
-
+        dotenv = Dotenv.load();
+        persistence = new MemoryPersistence();
+        connect(dotenv.get("BROKER"), dotenv.get("CLIENT_ID"));
     }
 
-    private void configClient (String broker, String clientId) {
-
-    }
-
-    public void sendNachricht (String topic, String content, int qos) {
-
-
+    private void connect (String broker, String clientId) {
         try {
-            MqttClient client = new MqttClient("tcp://127.0.0.1", "test", new MemoryPersistence());
+            client = new MqttClient(broker, clientId, persistence);
             MqttConnectOptions connOpts = new MqttConnectOptions();
             connOpts.setCleanSession(false);
             connOpts.setKeepAliveInterval(15);
             connOpts.setConnectionTimeout(30);
             client.connect();
-            MqttMessage message = new MqttMessage(content.getBytes());
-            message.setQos(qos);
-            message.setRetained(true);
-            client.publish(topic, message);
-            client.disconnect();
-        } catch (MqttException me) {
-            System.out.println("reason "+me.getReasonCode());
-            System.out.println("msg "+me.getMessage());
-            System.out.println("loc "+me.getLocalizedMessage());
-            System.out.println("cause "+me.getCause());
-            System.out.println("excep "+me);
-            me.printStackTrace();
+        } catch (MqttException e) {
+            e.printStackTrace();
         }
 
     }
 
+    public void sendNachricht (String topic, String content, int qos) {
+
+        try {
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            message.setRetained(true);
+            client.publish(topic, message);
+        } catch (MqttException me) {
+            me.printStackTrace();
+        }
+    }
 }
