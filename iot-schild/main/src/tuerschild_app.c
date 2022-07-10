8a5870d5130826b210ed25a2f6f12ea7ece96c16
Michael Kremser <michael_frederik.kremser@smail.th-koeln.de>
2022-07-10 03:09:44 +0200

    Fertig Dokumentiert und alle Testfälle erfolgreich

--- iot-schild/main/src/tuerschild_app.c
+++ iot-schild/main/src/tuerschild_app.c
@@ -13,8 +13,8 @@ void app_main(void)
 {
 	early_init();
 
-	const char* json = "{\"meeting1\":{ \"titel\":\"Meeting 1 \", \"uhrzeit\":\"14:00-15:00\"}, \"meeting2\":{ \"titel\":\"Meeting 2 \", \"uhrzeit\":\"15:00-16:00\"}, \"systeminfo\":{ \"datum\":\"04.07.2022 \", \"updateUhrzeit\":\"15:42\"}, \"meeting3\":{ \"titel\":\"Meeting 3 \", \"uhrzeit\":\"17:00-18:00\"}, \"meeting4\":{ \"titel\":\"Meeting 4 \", \"uhrzeit\":\"18:00-19:00\"}, \"aktuellesMeeting\":{ \"titel\":\"Abschlussvortrag \", \"verantwortlicher\":\"Stefan Slooten\", \"uhrzeit\":\"13:00-14:00\"}}";
-	const char* topic = "Hot Topic";
+	const char* json = "{\"meeting1\":{ \"titel\":\"12345678901234567890123456789012345678901234567890123456789012345678901234567890 \", \"uhrzeit\":\"14:00-15:00\"}, \"meeting2\":{ \"titel\":\"Meeting 2 \", \"uhrzeit\":\"15:00-16:00\"}, \"systeminfo\":{ \"datum\":\"04.07.2022 \", \"updateUhrzeit\":\"15:42\"}, \"meeting3\":{ \"titel\":\"Meeting 3 \", \"uhrzeit\":\"17:00-18:00\"}, \"meeting4\":{ \"titel\":\"Meeting 4 \", \"uhrzeit\":\"18:00-19:00\"}, \"aktuellesMeeting\":{ \"titel\":\"12345678901234567890123456789 \", \"verantwortlicher\":\"1234567890123456789012345678901234567890\", \"uhrzeit\":\"13:00-14:00\"}}";
+	const char* topic = "123456789";
 	
 	strcpy(topic_in_buf, topic);
 	strcpy(data_in_buf, json);
--- iot-schild/main/src/tuerschild_eink_esp32.cpp
+++ iot-schild/main/src/tuerschild_eink_esp32.cpp
@@ -9,6 +9,7 @@ extern "C" {
 #include "EPD.h"
 #include "GUI_Paint.h"
 
+//Nicht mehr benötigte Setup Funktion
 extern "C" void setup_display()
 {
     /*DEV_Module_Init();
@@ -17,15 +18,17 @@ extern "C" void setup_display()
     
 }
 
+//Funktion zur Erstellung der optischen Darstellung des Displays
 extern "C" void process_and_show()
 {
     initArduino();
 
+    //Initialisierung für Das E-Ink Display mit den Massen 7,5 Inch
     DEV_Module_Init();
     EPD_7IN5_V2_Init();
     EPD_7IN5_V2_Clear();
 
-
+    //Speicherreservierung des Bildes
     char* json = data_in_buf;
     UWORD Imagesize = ((EPD_7IN5_V2_WIDTH % 8 == 0) ? (EPD_7IN5_V2_WIDTH / 8) : (EPD_7IN5_V2_WIDTH / 8 + 1)) * EPD_7IN5_V2_HEIGHT;
     uint8_t* BlackImage = (UBYTE *)malloc(Imagesize);
@@ -37,10 +40,25 @@ extern "C" void process_and_show()
     //char json[]= "{\"meeting1\":{ \"titel\":\"Meeting 1 \", \"uhrzeit\":\"14:00-15:00\"}, \"meeting2\":{ \"titel\":\"Meeting 2 \", \"uhrzeit\":\"15:00-16:00\"}, \"systeminfo\":{ \"datum\":\"04.07.2022 \", \"updateUhrzeit\":\"15:42\"}, \"meeting3\":{ \"titel\":\"Meeting 3 \", \"uhrzeit\":\"17:00-18:00\"}, \"meeting4\":{ \"titel\":\"Meeting 4 \", \"uhrzeit\":\"18:00-19:00\"}, \"aktuellesMeeting\":{ \"titel\":\"Abschlussvortrag \", \"verantwortlicher\":\"Stefan Slooten\", \"uhrzeit\":\"13:00-14:00\"}}";
     
     DynamicJsonDocument doc(2048);
-    deserializeJson(doc, json);  
-                    
+    deserializeJson(doc, json); 
 
-    // 2.Drawing on the image
+    //Kürzung von Strings, die zu lang für den vorgesehenen Platz auf dem Display sind:
+    //Kürzung vom Titel des aktuellen Meetings     
+    char aktMeetTit[100];
+    strcpy(aktMeetTit, doc["aktuellesMeeting"]["titel"]);
+    aktMeetTit[29]=0;
+    //Kürzung vom Verantwortlichen des aktuellen Meetings
+    char aktMeetVer[100];
+    strcpy(aktMeetVer, doc["aktuellesMeeting"]["verantwortlicher"]);
+    aktMeetVer[28]=0;
+    //Kürzung vom Topic
+    char mytopic[100];
+    strcpy(mytopic, topic_in_buf);
+    mytopic[7]=0;    
+            
+
+    // Bild beschriften:
+    // optisch ansprechende Einteilung in Regionen
     printf("Drawing:BlackImage\r\n");
     Paint_DrawRectangle(0, 0, 800, 200, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
     TUERSCHILD_LOGI(tag, "drew big rect");
@@ -48,6 +66,8 @@ extern "C" void process_and_show()
     TUERSCHILD_LOGI(tag, "cleared rect");
     Paint_DrawCircle(685, 115, 70, WHITE, DOT_PIXEL_3X3, DRAW_FILL_EMPTY);
     TUERSCHILD_LOGI(tag, "cleared circle");
+
+    //Füllen der oberen Hälfte des Displays
     Paint_DrawString_EN(30, 20, doc["systeminfo"]["datum"], &Font20, BLACK, WHITE);
     TUERSCHILD_LOGI(tag, "wrote date");
     Paint_DrawString_EN(380, 20, "letzte Aktualisierung:", &Font20, BLACK, WHITE);                
@@ -55,19 +75,19 @@ extern "C" void process_and_show()
     Paint_DrawString_EN(700, 20, doc["systeminfo"]["updateUhrzeit"], &Font20, BLACK, WHITE);
     TUERSCHILD_LOGI(tag, "wrote uddate time");
     if(doc["aktuellesMeeting"]["titel"]) {
-        Paint_DrawString_EN(30, 70, doc["aktuellesMeeting"]["titel"], &Font24, WHITE, BLACK);
+        Paint_DrawString_EN(30, 70, aktMeetTit, &Font24, WHITE, BLACK);
     }
-    TUERSCHILD_LOGI(tag, "wrote current meeting");
-    //Paint_DrawString_EN(30, 110, "Meeting: Abschlussvortrag IoT Tuerschild", &Font16, WHITE, BLACK);
-
+    TUERSCHILD_LOGI(tag, "wrote current meeting");    
     String str1 = "Verantwortlicher: ";
-    String str2 = doc["aktuellesMeeting"]["verantwortlicher"];
+    String str2 = aktMeetVer;
     str1.concat(str2);
     Paint_DrawString_EN(30, 130,  str1.c_str(), &Font16, WHITE, BLACK);
     TUERSCHILD_LOGI(tag, "wrote verantwortlicher");
     Paint_DrawString_EN(30, 150, "Uhrzeit:", &Font16, WHITE, BLACK);
     Paint_DrawString_EN(130, 150, doc["aktuellesMeeting"]["uhrzeit"], &Font16, WHITE, BLACK);
     TUERSCHILD_LOGI(tag, "wrote akutelles meeting time");
+
+    //Füllen der unteren Hälfte des Displays
     Paint_DrawString_EN(30, 210, "Folgende Meetings:", &Font24, WHITE, BLACK);
     Paint_DrawString_EN(30, 260, doc["meeting1"]["uhrzeit"], &Font20, WHITE, BLACK);
     Paint_DrawString_EN(200, 260, doc["meeting1"]["titel"], &Font20, WHITE, BLACK);
@@ -79,11 +99,10 @@ extern "C" void process_and_show()
     Paint_DrawString_EN(200, 340, doc["meeting3"]["titel"], &Font20, WHITE, BLACK);
     TUERSCHILD_LOGI(tag, "wrote following meeting4");
     Paint_DrawString_EN(30, 380, doc["meeting4"]["uhrzeit"], &Font20, WHITE, BLACK);
-    Paint_DrawString_EN(200, 380, doc["meeting4"]["titel"], &Font20, WHITE, BLACK);              
-    //Paint_DrawNum(110, 20, 2022, &Font20, WHITE, BLACK);                               
-    Paint_DrawString_EN(630, 100, topic_in_buf, &Font24, BLACK, WHITE);
-    //Paint_DrawNum(670, 125, 12, &Font24, WHITE, BLACK);
+    Paint_DrawString_EN(200, 380, doc["meeting4"]["titel"], &Font20, WHITE, BLACK);
+    Paint_DrawString_EN(630, 100, mytopic, &Font24, BLACK, WHITE);
 
+    //Displayausgabe
     printf("EPD_Display\r\n");
     EPD_7IN5_V2_Display(BlackImage);
 
